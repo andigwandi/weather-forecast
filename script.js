5d6dd062f6904ec2dc5b12c8f79c9ff775943955
Sanjeev Kumar <sankumar31@publicisgroupe.net>
2022-09-29 15:15:38 +0530

    to Capital case

--- script.js
+++ script.js
@@ -8,35 +8,44 @@ let loc = document.querySelector(".location");
 let icon = document.querySelector(".icon");
 const kelvin = 273;
 window.addEventListener("load", () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position);
-        lon = position.coords.longitude;
-        lat = position.coords.latitude;
-    
-        // API ID
-        const api = "6d055e39ee237af35ca066f35474e9df";
-    
-        // API URL
-        const base =
-  `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
-  `lon=${lon}&appid=6d055e39ee237af35ca066f35474e9df`;
-    
-        // Calling the API
-        fetch(base)
-          .then((response) => {
-            return response.json();
-          })
-          .then((data) => {
-            console.log(data);
-            temperature.textContent = 
-                Math.floor(data.main.temp - kelvin) + "°C";
-            summary.textContent = data.weather[0].description;
-            loc.textContent = data.name + "," + data.sys.country;
-            let icon1 = data.weather[0].icon;
-            icon.innerHTML = 
-                `<img src="http://openweathermap.org/img/w/${icon1}.png" />`;
-          });
-      });
-    }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position);
+      lon = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      // API ID
+      const api = "6d055e39ee237af35ca066f35474e9df";
+
+      // API URL
+      const base =
+        `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
+        `lon=${lon}&appid=6d055e39ee237af35ca066f35474e9df`;
+
+      // Calling the API
+      fetch(base)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          temperature.textContent =
+            Math.floor(data.main.temp - kelvin) + "°C";
+          summary.textContent = camelCase(data.weather[0].description);
+          loc.textContent = data.name + "," + data.sys.country;
+          let icon1 = data.weather[0].icon;
+          icon.innerHTML =
+            `<img src="http://openweathermap.org/img/w/${icon1}.png" />`;
+        });
+    });
+  }
+});
+
+function camelCase(str) {
+  var out = "";
+  str.split(" ").forEach(function (el, idx) {
+    var add = el.toLowerCase();
+    out += (add[0].toUpperCase() + add.slice(1) + " ");
   });
+  return out;
+}
